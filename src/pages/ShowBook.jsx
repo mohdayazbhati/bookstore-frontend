5bbbbcf1d77c37735c0012d1aca79ec5272a2da0
mohdayazbhati <mohdayazbhati88@gmail.com>
2024-03-02 08:22:19 +0530

    sag

--- src/pages/ShowBook.jsx
+++ src/pages/ShowBook.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
 
 const ShowBook = () => {
+  const [book, setBook] = useState({});
+  const [loading, setLoading] = useState(false);
+const {id} = useParams();
+
+
+useEffect(() => {
+  setLoading(true);
+  axios
+    .get(`http://localhost:5555/books/${id}`)
+    .then((response) => {
+        setBook(response.data);
+        setLoading(false)
+    })
+    .catch((error) => {
+      console.log(error)
+      
+    })
+}, [])
   return (
     <div>
       
